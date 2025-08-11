d4af67ad714cd0da1e5c639f1dc171239d3e3a09
Mohit Garg <mohitgarg@Mohits-MacBook-Air-3.local>
2025-08-11 22:06:30 +0530

    assignment1

---
+++ DeleteDuplicates.java
@@ -0,0 +1,18 @@
+public class DeleteDuplicates {
+    int length;
+    int array[];
+    DeleteDuplicates(int length)
+    {
+        this.length=length;
+        this.array = new int[length];
+    }
+    void DeleteElements()
+    {
+        
+    }
+    public static void main(String[] args) {
+        DeleteDuplicates obj1 = new DeleteDuplicates(10);
+
+    }
+}
+
