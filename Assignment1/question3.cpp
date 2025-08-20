e62ea548ab81eb81b0e1a822010ebe9ba4ef3e3a
MohitGarg92 <mohuji9@gmail.com>
2025-08-21 00:38:57 +0530

    Update question3.cpp

--- Assignment1/question3.cpp
+++ Assignment1/question3.cpp
@@ -5,6 +5,6 @@ int i;
 int arr[5] = {1};
 for (i = 0; i < 5; i++)
 printf("%d",arr[i]);
-//the code will result in all the elements as 0 
+//the code will result in all the remaining elements as 0 therefore 10000
 return 0;
 }
