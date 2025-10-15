978d60f97dd01be64ae948e8a51be40f39a187de
MohitGarg92 <mohuji9@gmail.com>
2025-10-15 22:02:20 +0530

    Create Question4.cpp

---
+++ Assignment2/Question4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s1="mohit";
+    string s2="garg";
+    string add = s1+" "+s2;
+    cout<<add<<endl;
+    return 0;
+    
+}
